bc480307616f57c547cc488a3710bef8af079a62
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2024-05-07 08:14:19 -0300

    Update atividade 3.java

--- atividades - Java/atividade 3.java
+++ atividades - Java/atividade 3.java
@@ -8,17 +8,22 @@ public class Main {
         int nota = scanner.nextInt();
         
         if (nota >= 90 && nota <= 100) {
-            System.out.println("A nota do aluno é A.");
-        } else if (nota >= 80 && nota < 89) {
-            System.out.println("A nota do aluno é B.");
-        } else if (nota >= 70 && nota < 79) {
-            System.out.println("A nota do aluno é C.");
-        } else if (nota >= 60 && nota < 69) {
-            System.out.println("A nota do aluno é D.");
-        } else if (nota >= 0 && nota < 59) {
-            System.out.println("A nota do aluno é F.");
-        } else {
-            System.out.println("Nota inválida. Por favor, insira um valor entre 0 e 100.");
+        System.out.println("A nota do aluno é A.");
+        } 
+        else if (nota >= 80 && nota < 89) {
+        System.out.println("A nota do aluno é B.");
+        } 
+        else if (nota >= 70 && nota < 79) {
+        System.out.println("A nota do aluno é C.");
+        } 
+        else if (nota >= 60 && nota < 69) {
+        System.out.println("A nota do aluno é D.");
+        } 
+        else if (nota >= 0 && nota < 59) {
+        System.out.println("A nota do aluno é F.");
+        } 
+        else {
+        System.out.println("Nota inválida. Por favor, insira um valor entre 0 e 100.");
         }
     
     }
